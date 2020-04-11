2eb564ff2817c53914d369e9b3a85858d46ee5e9
Jonatas Francklin Yano de Sousa <jonatasyano@Jonatass-MacBook-Pro.local>
2020-04-11 16:30:58 +0200

    teste2

--- index.js
+++ index.js
@@ -60,11 +60,12 @@ const argv = yargs
 
     //mais um teste
 
-    //mais um teste
+    //mais um teste2
 
-    //mais um teste
+    //mais um teste2
 
-    //mais um teste
+    //mais um teste2
 
-    //mais um teste
+    //mais um teste2
+    //mais um teste2
 
